2d49e4431957bbfdd2fd18efa5c59686dfb1f016
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2023-01-23 20:35:14 -0800

    added my code from previous code into express

--- express-server.js
+++ express-server.js
@@ -7,17 +7,35 @@ const port = 8000;
 
 // Get JSON
 app.get("/", (req, res) => {
-  res.send("GET JSON");
+  const responseData = {
+    message: "Hello, Chris",
+    endingMessage: "Thank you",
+  };
+
+  const jsonContent = JSON.stringify(responseData);
+  res.end(jsonContent);
 });
 
 // Post JSON
 app.post("/", (req, res) => {
-  res.send("POST JSON");
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(`{"message": "This is a JSON Response"}`);
 });
 
 // Get Blocking JSON
 app.get("/blocking", (req, res) => {
-  res.send("Blocking");
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url === "/blocking") {
+    let sum = 0;
+    for (let i = 1; i <= 100000000; i++) {
+      sum = sum + i;
+    }
+    res.end(`Finished Blocking ${Math.random()}`);
+  } else {
+    res.end(`Finished Non Blocking ${Math.random()}`);
+  }
 });
 
 app.listen(port, host, () => {
