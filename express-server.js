0d02272e8667b1647ed9af8e6031b45e0ad384a6
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-01-24 09:12:16 -0800

    code challenge with chris before typescript

--- express-server.js
+++ express-server.js
@@ -12,31 +12,33 @@ app.get("/", (req, res) => {
     endingMessage: "Thank you",
   };
 
-  const jsonContent = JSON.stringify(responseData);
-  res.end(jsonContent);
+  res.status(400).json(responseData);
+  // res.json(responseData);
+  // .json would also "send" or "end" the stream
+  // typically do not want to use end in express
+  // send would send the client the request
 });
 
 // Post JSON
-app.post("/", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200);
-  res.end(`{"message": "This is a JSON Response"}`);
-});
+function callBack(req, res) {
+  res.json({ message: "This is a JSON Response" });
+}
+app.post("/", callBack);
+// automatically send a 200 response unless i specifically set the status
+// very good pattern to follow for large code projects
+// Code split
 
 // Get Blocking JSON
 app.get("/blocking", (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
-  if (req.url === "/blocking") {
-    let sum = 0;
-    for (let i = 1; i <= 100000000; i++) {
-      sum = sum + i;
-    }
-    res.end(`Finished Blocking ${Math.random()}`);
-  } else {
-    res.end(`Finished Non Blocking ${Math.random()}`);
+  let sum = 0;
+  for (let i = 1; i <= 100000000; i++) {
+    sum = sum + i;
   }
+  res.end(`Finished Blocking ${Math.random()}`);
 });
+// tried to avoid this when using node 
 
 app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
